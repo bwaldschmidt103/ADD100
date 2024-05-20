aaa38b93bf0a9d6e66bbaa3659b75a0fe62f4c4b
Brian Waldschmidt <159931728+bwaldschmidt103@users.noreply.github.com>
2024-05-20 16:17:25 -0500

    Add files via upload

---
+++ 1-2.2/understanding-data-types-in-python.py
@@ -0,0 +1,21 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 9
+print("Literal:", x)
+
+# Integer
+y = -1111
+print("Integer:", y)
+
+# Float
+z = 33.3
+print("Float:", z)
+
+# String
+name = "Barik"
+print("String:", name)
+
+# Boolean
+is_active = True
+print("Boolean:", is_active)
