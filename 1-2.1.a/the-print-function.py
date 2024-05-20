c20da7eea247a14ff9ee640834df7cb46075e1f8
Brian Waldschmidt <159931728+bwaldschmidt103@users.noreply.github.com>
2024-05-20 14:49:31 -0500

    Add files via upload

---
+++ 1-2.1.a/the-print-function.py
@@ -0,0 +1,10 @@
+# Knock-Knock Joke Program
+print("Knock, knock.")
+print("Who's there?")
+print("Wooden shoe.")
+print("Wooden shoe who?")
+print("Wooden shoe like to hear more jokes?")
+print("")
+
+# As one line of code using escape characters
+print("Knock, knock. \nWho’s there?\nWooden shoe.\nWooden shoe who?\nWooden shoe like to hear more jokes?")
