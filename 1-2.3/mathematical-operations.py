0c20dee537e226f739f3a9e1cd37aac234d5ee19
Brian Waldschmidt <159931728+bwaldschmidt103@users.noreply.github.com>
2024-05-21 15:11:07 -0500

    Add files via upload

---
+++ 1-2.3/mathematical-operations.py
@@ -0,0 +1,7 @@
+# A Python Program that Calculates the area of a rectangle
+
+length = 24
+width = 33
+area = length * width
+
+print("The area of the rectangle with a length of " + str(length) + " and a width of " + str(width) + " is " + str(area) + ".")
