3ea96316c95b92f800a42f89cf513398adfacae8
Brian Waldschmidt <159931728+bwaldschmidt103@users.noreply.github.com>
2024-06-11 15:35:43 -0500

    Add files via upload

---
+++ pe1_4.1/functions.py
@@ -0,0 +1,15 @@
+
+# Define the function to calculate the area of a square
+def square(side):
+    area = side * side
+    print(f"The area of the square is {str(area)} square units.")
+
+#Define the function to calculate the area of a circle
+def circle(radius):
+    pi = 3.14
+    area = pi * radius * radius
+    print(f"The area of the circle is {str(area)} square units.")
+
+# Test the functions with example values
+square(18)
+circle(15)
