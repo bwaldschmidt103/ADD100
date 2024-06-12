4a0a42516bac5e4a57d888188afad8611c7cbfa7
Brian Waldschmidt <159931728+bwaldschmidt103@users.noreply.github.com>
2024-06-12 16:05:57 -0500

    Add files via upload

---
+++ pe1_4.2/madlibs.py
@@ -0,0 +1,55 @@
+def madlibs(verb, adjective, person, verb_2, noun, noun_2, color, emotion):
+    # my madlibs
+
+
+    print(f"Oh, I could hide 'neath the wings")
+    print(f"Of the bluebird as she sings")
+    print(f"The six o'clock alarm would never ring")
+    print(f"But it rings and I rise")
+    print(f"Wipe the sleep out of my eyes")
+    print(f"My shavin' razor's cold and it stings")
+    print("\n") # adding a space
+    print(f"{verb} up, {adjective} {person}")
+    print(f"Oh, what can it {verb_2}")
+    print(f"To a {noun} believer")
+    print(f"And a {noun_2} queen?")
+    print("\n") # adding a space
+    print(f"You once thought of me")
+    print(f"As a {color} knight on his steed")
+    print(f"Now you know how {emotion} I can be")
+    print(f"Oh, and our good times starts and end")
+    print(f"Without dollar one to spend")
+    print(f"But how much, baby, do we really need")
+    print("\n") # adding a space
+    print(f"{verb} up, {adjective} {person}")
+    print(f"Oh, what can it {verb_2}")
+    print(f"To a {noun} believer")
+    print(f"And a {noun_2} queen")
+    print(f"{verb} up, {adjective} {person}")
+    print(f"Oh, what can it {verb_2}")
+    print(f"To a {noun} believer")
+    print(f"And a {noun_2} queen")
+    print("\n") # adding a space
+    print(f"{verb} up, {adjective} {person}")
+    print(f"Oh, what can it {verb_2}")
+    print(f"To a {noun} believer")
+    print(f"And a {noun_2} queen")
+    print(f"{verb} up, {adjective} {person}")
+    print(f"Oh, what can it {verb_2}")
+    print(f"To a {noun} believer")
+    print(f"And a {noun_2} queen")
+    print("\n") # adding a space
+
+
+verb = input("Please enter a verb:  ")
+adjective = input("Please enter an adjective:  ")
+person = input("Please enter the name of a person:  ")
+verb_2 = input("Please enter another verb:  ")
+noun = input("Please enter a noun:  ")
+noun_2 = input("Please enter another noun:  ")
+color = input("Please enter a color:  ")
+emotion = input("Please enter an emotion:  ")
+print("\n") # adding a space
+
+
+madlibs(verb, adjective, person, verb_2, noun, noun_2, color, emotion)
